7d6868c9529e5020c76b00c1fe2740609fe62492
Murxli <murxli.krishna@gmail.com>
2025-08-05 22:32:16 +0530

    added changes in website

---
+++ src/components/CancellationRefund.js
@@ -0,0 +1,4 @@
+const CancellationRefund = () => {
+  return <div><h1>Cancellation & Refund Policy</h1><p>This is a placeholder Cancellation and Refund Policy.</p></div>;
+};
+export default CancellationRefund;
